d0fd79456beec55ccc34ed6d895f54715dfcc482
DalexHD <eldalexhd@gmail.com>
2021-12-16 21:04:49 +0000

    feat: update main.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 12:36:41 by aborboll          #+#    #+#             */
-/*   Updated: 2021/12/16 19:43:51 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/12/16 21:04:38 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,15 @@ int main(void)
 {
 	Tester * tests[] = { new is_integral_tester() };
 
-	for (size_t i = 0; i < 1; i++)
+	size_t	i = 0;
+
+	while (i < 1)
 	{
 		if (!tests[i]->test())
 			exit_status = 1;
 		delete tests[i];
+		i++;
 	}
+
 	return (exit_status);
 }
