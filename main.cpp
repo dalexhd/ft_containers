9a00833241b44cec9059b8b2fce6624177293c36
DalexHD <eldalexhd@gmail.com>
2021-12-16 12:40:17 +0000

    feat: add files.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 12:36:41 by aborboll          #+#    #+#             */
+/*   Updated: 2021/12/16 12:38:25 by aborboll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Incluimos las librerias esenciales.
+*/
+#include <iostream>
+#include <string.h>
+
+int main(void)
+{
+	std::cout << "Hello world!" << std::endl;
+	return (0);
+}
